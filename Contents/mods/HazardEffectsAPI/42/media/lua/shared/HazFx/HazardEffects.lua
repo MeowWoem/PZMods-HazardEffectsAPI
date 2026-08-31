c393c18924bb4157081f5ff438fbb7ec76c47a67
Amenophis <l.stoller@outlook.fr>
2026-08-31 17:40:01 +0200

    Adding some logging function

--- Contents/mods/HazardEffectsAPI/42/media/lua/shared/HazFx/HazardEffects.lua
+++ Contents/mods/HazardEffectsAPI/42/media/lua/shared/HazFx/HazardEffects.lua
@@ -2,3 +2,18 @@ HazardEffects = HazardEffects or {};
 
 HazardEffects.__index = HazardEffects;
 
+function HazardEffects.debug(...)
+    if(isDebugEnabled()) then
+        print("HazFX", ...);
+    end
+end
+function HazardEffects.warn(...)
+    print("[WARN] HazFX", ...);
+end
+function HazardEffects.err(...)
+    print("[ERR] HazFX", ...);
+end
+
+hazlog = HazardEffects.debug;
+hazwarn = HazardEffects.warn;
+hazerr = HazardEffects.err;
