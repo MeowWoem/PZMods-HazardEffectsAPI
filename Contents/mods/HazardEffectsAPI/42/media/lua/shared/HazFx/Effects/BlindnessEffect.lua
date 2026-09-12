10dcb1ac3edaae0584be28758d2a49b583f58dc8
Amenophis <l.stoller@outlook.fr>
2026-09-12 06:59:33 +0200

    Start to implement the BlindnessEffect

--- Contents/mods/HazardEffectsAPI/42/media/lua/shared/HazFx/Effects/BlindnessEffect.lua
+++ Contents/mods/HazardEffectsAPI/42/media/lua/shared/HazFx/Effects/BlindnessEffect.lua
@@ -1,47 +1,73 @@
-require("HazFx/EffectRegistry");
-require("HazFx/Effects/BaseEffect");
+local EffectRegistry = require("HazFx/EffectRegistry");
+local BaseEffect = require("HazFx/Effects/BaseEffect");
+local Utils = require("HazFx/Utils");
 
-local BaseEffect = HazardEffects.Effects.BaseEffect;
+HazardEffects.Effects.BlindnessEffect = HazardEffects.Effects.BlindnessEffect or BaseEffect:derive("BlindnessEffect");
 
-HazardEffects.Effects.BlindnessEffect = HazardEffects.Effects.BlindnessEffect
-    or BaseEffect:derive("BlindnessEffect");
 
+---@class BlindnessEffect : BaseEffect
 local BlindnessEffect = HazardEffects.Effects.BlindnessEffect;
 
 function BlindnessEffect:new(player, playerNum, playerOnlineID)
-    local o = BaseEffect.new(self, player, playerNum, playerOnlineID);
-    return o;
+    return BaseEffect.new(self, player, playerNum, playerOnlineID);
 end
 
-function BlindnessEffect:init()
-    local md = self:getModData();
+function BlindnessEffect:initialize()
+    self:definePersistant("radius", 0);
+    self:definePersistant("isIlliterate", self.player:hasTrait(CharacterTrait.ILLITERATE));
 
-    md.radius = md.radius or 0;
-    md.isIlliterate = md.isIlliterate or self.player:hasTrait(CharacterTrait.ILLITERATE);
+    self.fxData = {
+        blur = 1,
+        desat = 10,
+        darkness = 1,
+        gw = 4
+    };
 
-    self.radius = md.radius;
-    self.isIlliterate = md.isIlliterate;
+    self:initSearchManager();
 
 end
 
-function BlindnessEffect:activate(duration)
-    self.modData.isActive = true;
-    self.isActive = true;
+function BlindnessEffect:initSearchManager()
+    if(self._searchManager == nil and Utils.isClient()) then
+        local sm = getSearchMode();
+        if(sm) then
+            self._searchManager = sm:getSearchModeForPlayer(self.playerNum);
+            self._searchMode = sm;
+        end
+    end
+    return self._searchManager;
+end
+
+function BlindnessEffect:getSearchManager()
+    return self:initSearchManager();
 end
 
 function BlindnessEffect:deactivate()
-    self.modData.isActive = false;
-    self.isActive = false;
+    BaseEffect.deactivate(self);
+    self._searchMode:setEnabled(self.playerNum, false);
 end
 
-function BlindnessEffect:onPlayerUpdate()
+function BlindnessEffect:activate(duration, radius)
+    self.radius = radius;
+    BaseEffect.activate(self, duration);
+end
+
+function BlindnessEffect:render()
+    BaseEffect.render(self);
     if not self.isActive then return; end
-    
+    local radius = PZMath.lerp(self.radius, 25, self.timeElapsed / self.duration);
+    self._searchManager:getRadius():setTargets(radius, radius);
+    self._searchManager:getBlur():setTargets(self.fxData.blur, self.fxData.blur);
+    self._searchManager:getDesat():setTargets(self.fxData.desat, self.fxData.desat);
+    self._searchManager:getDarkness():setTargets(self.fxData.darkness, self.fxData.darkness);
+    self._searchManager:getGradientWidth():setTargets(self.fxData.gw, self.fxData.gw);
+    self._searchMode:setEnabled(self.playerNum, true);
 end
 
 function BlindnessEffect:everyOneMinute()
+    BaseEffect.everyOneMinute(self);
     if not self.isActive then return; end
     
 end
 
-HazardEffects.EffectRegistry.getInstance():register(BlindnessEffect);
+EffectRegistry.getInstance():register(BlindnessEffect);
