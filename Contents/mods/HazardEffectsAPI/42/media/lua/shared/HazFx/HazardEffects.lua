10ac9f4f94ee80d762e2bcd6c1ba6108783eae37
Amenophis <l.stoller@outlook.fr>
2026-09-12 07:01:05 +0200

    Add a function to get Mod Data for a IsoObject

--- Contents/mods/HazardEffectsAPI/42/media/lua/shared/HazFx/HazardEffects.lua
+++ Contents/mods/HazardEffectsAPI/42/media/lua/shared/HazFx/HazardEffects.lua
@@ -2,6 +2,14 @@ HazardEffects = HazardEffects or {};
 
 HazardEffects.__index = HazardEffects;
 
+HazardEffects.MOD_DATA_KEY = "HazardEffectsAPI";
+
+function HazardEffects.getModData(isoObj)
+    local md = isoObj:getModData();
+    md[HazardEffects.MOD_DATA_KEY] = md[HazardEffects.MOD_DATA_KEY] or {};
+    return md[HazardEffects.MOD_DATA_KEY];
+end
+
 function HazardEffects.debug(...)
     if(isDebugEnabled()) then
         print("HazFX", ...);
@@ -11,9 +19,11 @@ function HazardEffects.warn(...)
     print("[WARN] HazFX", ...);
 end
 function HazardEffects.err(...)
-    print("[ERR] HazFX", ...);
+    error(table.concat({"[ERR] HazFX", ...}, ' '));
 end
 
 hazlog = HazardEffects.debug;
 hazwarn = HazardEffects.warn;
 hazerr = HazardEffects.err;
+
+return HazardEffects;
